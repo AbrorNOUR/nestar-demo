6bf3da05ba9429c057303f70a4dcc82b714b204c
AbrorNOUR <abrorjonfayzullaev@gmail.com>
2025-10-16 03:20:21 +0900

    fix: refactor pages components

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,31 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Stack className={"home-page"}>
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Agents</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Properties</Stack>
-        </Stack>
+    <Stack className="home-page">
+      {/* Trend Properties */}
+      <Stack className="container section">
+        <h2>Trend Properties</h2>
+      </Stack>
+
+      {/* Popular Properties */}
+      <Stack className="container section">
+        <h2>Popular Properties</h2>
+      </Stack>
+
+      {/* Advertisement */}
+      <Stack className="container section">
+        <h2>Advertisement</h2>
+      </Stack>
+
+      {/* Top Properties */}
+      <Stack className="container section">
+        <h2>Top Properties</h2>
+      </Stack>
+
+      {/* Top Agents */}
+      <Stack className="container section">
+        <h2>Top Agents</h2>
+      </Stack>
     </Stack>
   );
 };
